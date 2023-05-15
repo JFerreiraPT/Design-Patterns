0fae1e75e73defad395a3575d273404e2f5da938
João Ferreira <joao.p.r.fereira.ofgmail.com>
2023-05-15 15:00:41 +0100

    add functional builder final code

--- Builder/FunctionalBuilder/Program.cs
+++ Builder/FunctionalBuilder/Program.cs
@@ -14,29 +14,67 @@ public class Person
     }
 }
 
-//Sealed prevents other classes from ingerit from it
-public sealed class PersonBuilder
+
+public abstract class FunctionalBuilder<TSubject, TSelf>
+    where TSelf : FunctionalBuilder<TSubject, TSelf>
+    //this means that TSubject should gave parameter-less constructor
+    //this way we can di TSubject subject = new TSubject() otherwise it woul't be possible
+    //We can combine multiple where T : class: new() -> should be a class and have an empty constructor
+    where TSubject: new()
 {
     private readonly List<Func<Person, Person>> actions
         = new List<Func<Person, Person>>();
 
-    public PersonBuilder Called(string name)
+    public TSelf Called(string name)
         => Do(p => p.Name = name);
 
-    public PersonBuilder Do(Action<Person> action) => AddAction(action);
+    public TSelf Do(Action<Person> action) => AddAction(action);
 
     public Person Build()
         => actions.Aggregate(new Person(), (p, f) => f(p));
 
-    private PersonBuilder AddAction(Action<Person> action)
+    private TSelf AddAction(Action<Person> action)
     {
-        actions.Add(p => { action(p);
+        actions.Add(p =>
+        {
+            action(p);
             return p;
         });
 
-        return this;
+        return (TSelf)this;
     }
 }
+
+public sealed class PersonBuilder
+    : FunctionalBuilder<Person, PersonBuilder>
+{
+
+}
+
+
+//Sealed prevents other classes from ingerit from it
+//public sealed class PersonBuilder
+//{
+//    private readonly List<Func<Person, Person>> actions
+//        = new List<Func<Person, Person>>();
+
+//    public PersonBuilder Called(string name)
+//        => Do(p => p.Name = name);
+
+//    public PersonBuilder Do(Action<Person> action) => AddAction(action);
+
+//    public Person Build()
+//        => actions.Aggregate(new Person(), (p, f) => f(p));
+
+//    private PersonBuilder AddAction(Action<Person> action)
+//    {
+//        actions.Add(p => { action(p);
+//            return p;
+//        });
+
+//        return this;
+//    }
+//}
 
 
 public static class PersonBuilderExtensions
