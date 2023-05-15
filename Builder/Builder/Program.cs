15f8743327f544c9f849371764378b0ce2054057
João Ferreira <joao.p.r.fereira.ofgmail.com>
2023-05-15 12:18:18 +0100

    add Fluent Builder code

--- Builder/Builder/Program.cs
+++ Builder/Builder/Program.cs
@@ -1,3 +1,92 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+
+var builder = new HtmlBuilder("ul");
+builder.AddChild("li", "Hello").AddChild("li", "world");
+Console.WriteLine(builder.ToString());
+
+
+
+
+
+
+
+
+
+
+public class HtmlElement
+{
+    public string Name, Text;
+    public List<HtmlElement> Elements = new List<HtmlElement>();
+    private const int identSize = 2;
+
+    public HtmlElement()
+    {
+    }
+
+    public HtmlElement(string name, string text)
+    {
+        Name = name;
+        Text = text;
+    }
+
+    private string ToStringImpl(int indent)
+    {
+        var sb = new StringBuilder();
+        var i = new string(' ', identSize * indent);
+        sb.AppendLine($"{i}<{Name}>");
+
+        if(!string.IsNullOrWhiteSpace(Text))
+        {
+            sb.Append(new string(' ', identSize * (indent + 1)));
+            sb.AppendLine(Text);
+        }
+
+
+        foreach (var e in Elements)
+        {
+            sb.Append(e.ToStringImpl(identSize + 1));
+            
+        }
+
+        sb.AppendLine($"{i}</{Name}>");
+
+        return sb.ToString();
+    }
+
+    public override string ToString()
+    {
+        return ToStringImpl(0);
+    }
+
+}
+
+public class HtmlBuilder
+{
+    HtmlElement root = new HtmlElement();
+    private string _rootName;
+
+    public HtmlBuilder(string rootName)
+    {
+        this._rootName = rootName;
+        root.Name = rootName;
+    }
+
+    public HtmlBuilder AddChild(string childName, string childText)
+    {
+        var e = new HtmlElement(childName, childText);
+        root.Elements.Add(e);
+        return this;
+    }
+
+    public override string ToString()
+    {
+        return root.ToString();
+    }
+
+    public void Clear()
+    {
+        root = new HtmlElement { Name = _rootName };
+    }
+}
 
---
+++ Builder/FluentBuilder/Program.cs
@@ -0,0 +1,56 @@
+﻿
+//person.New returns a builder
+var me = Person.New.Called("João Ferreira").WorkAsA("Developer").Build();
+Console.WriteLine(me);
+
+public class Person
+{
+    public string Name;
+    public string Position;
+
+    public class Builder : PersonJobBuilder<Builder>
+    {
+        
+    }
+
+    public static Builder New => new Builder();
+
+    public override string ToString()
+    {
+        return $"{nameof(Name)}: {Name}, {nameof(Position)}: {Position}";
+    }
+}
+
+public abstract class PersonBuilder
+{
+    protected Person person = new Person();
+    public Person Build()
+    {
+        return person;
+    }
+}
+
+public class PersonalInfoBuilder<SELF> : PersonBuilder
+    where SELF : PersonalInfoBuilder<SELF>
+{
+    
+
+    //Fluid method
+    public SELF Called(string name)
+    {
+        person.Name = name;
+        return (SELF)this;
+    }
+}
+
+//this examples serves to show the problem with ihneritance
+public class PersonJobBuilder<SELF> : PersonalInfoBuilder<PersonJobBuilder<SELF>>
+    where SELF : PersonJobBuilder<SELF>
+{
+    public SELF WorkAsA(string postion)
+    {
+        person.Position = postion;
+        return (SELF)this;
+    }
+}
+
