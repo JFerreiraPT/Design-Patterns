4eea23bc16cfcfea0a9df96d742a73d1d586bcf8
João Ferreira <joao.p.r.fereira.ofgmail.com>
2023-05-17 11:27:28 +0100

    feat: add bridge and composite code examples

---
+++ Composite/Composite/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+
+var drawing = new GraphicObject { Name = "My drawing" };
+drawing.Children.Add(new Square { Color = "Red" });
+drawing.Children.Add(new Circle { Color = "Yellow" });
+
+
+var group = new GraphicObject();
+group.Children.Add(new Square { Color = "Blue" });
+group.Children.Add(new Circle { Color = "Blue" });
+
+drawing.Children.Add(group);
+
+Console.WriteLine(drawing);
+
+
+
+
+public class GraphicObject
+{
+    public virtual string Name { get; set; } = "Group";
+    public string Color;
+
+    private Lazy<List<GraphicObject>> children = new Lazy<List<GraphicObject>>();
+    //Expose children, using lazy means that only when need the list will be instatiated
+    public List<GraphicObject> Children => children.Value;
+
+
+    //Build a composite tree of objects
+    private void Print(StringBuilder sb, int depth)
+    {
+        sb.Append(new string('*', depth))
+            .Append(string.IsNullOrWhiteSpace(Color) ? string.Empty : $"{Color} ")
+            .AppendLine(Name);
+
+        foreach (var child in Children)
+        {
+            child.Print(sb, depth + 1);
+        }
+    }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        //print top level
+        Print(sb, 0);
+        return sb.ToString();
+    }
+}
+
+public class Circle : GraphicObject
+{
+    public override string Name => "Circle";
+}
+
+public class Square : GraphicObject
+{
+    public override string Name => "Square";
+}
---
+++ Composite/NeuralNetworks/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+
+using System.Collections;
+using System.Collections.ObjectModel;
+
+var neuron1 = new Neuron();
+var neuron2 = new Neuron();
+
+neuron1.ConnectTo(neuron2);
+
+var layer1 = new NeuronLayer();
+var layer2 = new NeuronLayer();
+layer1.ConnectTo(layer2);
+
+neuron1.ConnectTo(layer1);
+
+public static class ExtensionMethods
+{
+    public static void ConnectTo(this IEnumerable<Neuron> self, IEnumerable<Neuron> other)
+    {
+        //iterate every neurons in self and any neuros on other and connect one to each other
+        if (ReferenceEquals(self, other)) return;
+        foreach (var from in self)
+        foreach (var to in other)
+        {
+                from.Out.Add(to);
+                to.In.Add(from);
+
+        }
+    }
+}
+
+public class Neuron : IEnumerable<Neuron>
+{
+    public float Value;
+    //We can a have connections coming in and connections comming out
+    public List<Neuron> In, Out;
+
+
+
+    public IEnumerator<Neuron> GetEnumerator()
+    {
+        yield return this;
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class NeuronLayer : Collection<Neuron>
+{
+
+}
